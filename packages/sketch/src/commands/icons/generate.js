d6fefb5b4683f3ec2f11e6230503c22d74872925
renmaddox <51000506+renmaddox@users.noreply.github.com>
2020-03-20 15:18:58 -0500

    Revert "fix(sketch): add undefined exception handler in icon generate script (#5583)"
    This reverts commit 83b766e7ef5aaeb4ae23d31a57a703690d05a15c.

--- packages/sketch/src/commands/icons/generate.js
+++ packages/sketch/src/commands/icons/generate.js
@@ -68,11 +68,6 @@ export function generate() {
             const [_type, _category, _subcategory, name, size] = parts;
             return name === icon && size === '32';
           });
-
-          if (!symbol) {
-            throw new Error(`Unable to find symbol for icon ${icon}!`);
-          }
-
           const instance = symbol.createNewInstance();
           instance.frame.offset(ICON_X_OFFSET, ICON_Y_OFFSET);
 
